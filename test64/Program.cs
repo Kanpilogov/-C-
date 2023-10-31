9aafb8f306f0cd76db5098a8bccfb5e0090f5042
Admim <nwspb2>
2023-10-31 21:47:16 +0300

    Zadanie sdelano

---
+++ test64/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите  натуральное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+
+if(numberM < 1 )
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+PrintRangeNaturalNumbers(numberM);
+
+void PrintRangeNaturalNumbers(int numM)
+{
+    if (numM < 1)
+    {
+        Console.Write($"{numM} ");
+        PrintRangeNaturalNumbers(numM + 1);
+    }
+
+    else if (numM > 1)
+    {
+        Console.Write($"{numM} ");
+        PrintRangeNaturalNumbers(numM - 1);
+    }
+    else Console.Write($"{numM} ");
+}
---
+++ test68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите натуральное число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+if (numM < 0 || numN < 0)
+{
+    Console.WriteLine("Нельзя вводить отрицателое число");
+    return;
+}
+
+
+int FAck(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return FAck(m - 1, 1);
+    return FAck(m - 1, FAck(m, n - 1));
+}
+
+int result = FAck(numM, numN);
+Console.WriteLine($"m={numM}, n={numN}-> A(m,n) = {result}");
+
